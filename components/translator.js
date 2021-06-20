ce128f2919006332bb5dad1afb8cbced18b1ce70
Felipe Villegas <felavid@gmail.com>
2021-06-19 20:54:29 -0400

    added comments/user stories

--- components/translator.js
+++ components/translator.js
@@ -3,6 +3,25 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+// Handle time: Ten thirty 
+// is written as "10.30" in British English 
+// and "10:30" in American English. 
+// The span element should wrap the entire 
+// time string, i.e. 
+// <span class="highlight">10:30</span>.
+
+// Handle titles/honorifics:
+// Doctor Wright is abbreviated as "Dr Wright" 
+// in British English and "Dr. Wright" 
+// in American English.
+
+// Wrap any translated spelling or terms with 
+// <span class="highlight">...</span> tags
+
+// If text requires no translation, 
+// return "Everything looks good to me!" 
+// for the translation value.
+
 class Translator {
 
 }
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,36 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+
+      if (!req.body.text || !req.body.locale) {
+        res.json({ error: 'Required field(s) missing' })
+        return
+      }
+      if (req.body.text.length === 0) {
+        res.json({ error: 'No text to translate' })
+        return
+      }
+      if (req.body.locale !== 'american-to-british' || req.body.locale !== 'british-to-american') {
+        res.json({ error: 'Invalid value for locale field' })
+        return
+      }
+
+      // Sample Request:
+      // {
+      // 	"text": "We watched the footie match for a while.",
+      // 	"locale": "british-to-american"
+      // }
+
+      // Sample Response:
+      // {
+      //   "text": "We watched the footie match for a while.",
+      //   "translation": "We watched the <span class=\"highlight\">soccer</span> match for a while."
+      // }
+
+      // If text requires no translation, 
+      // return "Everything looks good to me!" 
+      // for the translation value.
+      // Check if text === translation
+
     });
 };
